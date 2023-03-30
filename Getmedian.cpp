455f28064522cc42513ea6538acc48c4991e2784
JanakCSE210598B <129370684+JanakCSE210598B@users.noreply.github.com>
2023-03-30 17:20:21 +0530

    Update Getmedian.cpp

--- Getmedian.cpp
+++ Getmedian.cpp
@@ -1,26 +1,23 @@
-
 #include <iostream>
 #include <algorithm>
-#include<iomanip>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
     int n;
-    double median;
-
     cout << "Enter the number of elements in the array: ";
     cin >> n;
 
-    double arr[n];
+    int arr[n];
     cout << "Enter the elements of the array: ";
     for (int i = 0; i < n; i++)
         cin >> arr[i];
 
     sort(arr, arr + n);
 
-    
+    double median;
     if (n % 2 == 0)
         median = (arr[n/2 - 1] + arr[n/2]) / 2.0;
     else
