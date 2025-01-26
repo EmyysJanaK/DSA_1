11bd5bd072a1dea8a1c671ae8a264f914541cb02
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2025-01-27 05:05:00 +0530

    commit

--- AdvancedAlgorithms/week01/evacuation.cpp
+++ AdvancedAlgorithms/week01/evacuation.cpp
@@ -30,4 +30,16 @@ public:
   {
     edges.reserve(m * 2);
   }
+ void add_edge(int from, int to, int capacity)
+  {
+    /* Note that we first append a forward edge and then a backward edge,
+         * so all forward edges are stored at even indices (starting from 0),
+         * whereas backward edges are stored at odd indices in the list edges */
+    Edge forward_edge = {from, to, capacity, 0};
+    Edge backward_edge = {to, from, 0, 0};
+    graph[from].push_back(edges.size());
+    edges.push_back(forward_edge);
+    graph[to].push_back(edges.size());
+    edges.push_back(backward_edge);
+  }
 };
