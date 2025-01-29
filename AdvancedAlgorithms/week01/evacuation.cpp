ea9a38cddadda4e5b130eff86b641487c603e761
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2025-01-29 09:05:00 +0530

    commit

--- AdvancedAlgorithms/week01/evacuation.cpp
+++ AdvancedAlgorithms/week01/evacuation.cpp
@@ -111,3 +111,49 @@ void BFS(const FlowGraph &graph, int s, int t, vector<int> &pred)
     }
   }
 }
+
+int max_flow(FlowGraph &graph, int s, int t)
+{
+  int flow = 0;
+
+  /* Contains predecessors of a vertex to get
+     * the path and calculate minimum flow thereon. */
+  vector<int> pred(graph.size());
+
+  do
+  {
+
+    BFS(graph, s, t, pred);
+
+    if (pred[t] != -1)
+    {
+      int min_flow = numeric_limits<int>::max();
+
+      /* Find minimal flow on the path from BFS. */
+      for (int u = pred[t]; u != -1; u = pred[graph.get_edge(u).from])
+      {
+        min_flow = std::min(min_flow, graph.get_edge(u).capacity - graph.get_edge(u).flow);
+      }
+
+      /* Update flow in original and residual graphs along the path from BFS*/
+      for (int u = pred[t]; u != -1; u = pred[graph.get_edge(u).from])
+      {
+        graph.add_flow(u, min_flow);
+      }
+
+      flow += min_flow;
+    }
+
+  } while (pred[t] != -1);
+
+  return flow;
+}
+
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+  FlowGraph graph = read_data();
+
+  std::cout << max_flow(graph, 0, graph.size() - 1) << "\n";
+  return 0;
+}
